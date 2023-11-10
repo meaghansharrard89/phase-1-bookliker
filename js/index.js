4452be9ca4064ce3bbeff92540bffe70f588032c
Meaghan <meaghan.sharrard89@gmail.com>
2023-11-09 17:20:11 -0800

    made updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,83 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+});
+
+let bookList = []
+const url = 'http://localhost:3000/books';
+
+fetch('http://localhost:3000/books')
+.then(res => res.json())
+.then(book => {
+    bookList = book
+    loopThroughBooks(bookList)
+})
+
+function loopThroughBooks(book){
+    book.forEach(book => displayBookTitles(book))
+}
+
+function displayBookTitles(book){
+    const newList = document.createElement('li')
+    const list = document.getElementById('list-panel')
+    newList.innerHTML = book.title
+    list.append(newList)
+
+    newList.addEventListener('click', () => {
+        addNewBook(book.id)
+    })
+}
+
+function addNewBook(id){
+    fetch(`${url}/${id}`)
+    .then(res => res.json())
+    .then(data => displayBookInfo(data))
+}
+
+function displayBookInfo(book){
+    const showList = document.getElementById('show-panel')
+    const users = book.users
+    const button = document.createElement('button')
+    let userList = ''
+    let userArray = []
+    users.forEach(book => {
+        userList += `<li>${book.username}</li>`
+        userArray.push(book.username)
+    })
+
+    showList.innerHTML = `
+        <img src=${book.img_url} />
+        <h1>${book.title}</h1>
+        <h2>${book.subtitle}</h2>
+        <h3>${book.author}</h3>
+        <p>${book.description}</p>`
+        
+    const list = document.createElement('ul')
+    list.innerHTML = userList
+    showList.append(list)
+    
+    button.className = 'like-btn'
+    button.id = book.id
+    button.textContent = 'Like ❤️'
+    showList.append(button)
+
+    button.addEventListener('click', () => {
+        const newName = document.createElement('li')
+        newName.textContent = 'meaghan'
+        list.append(newName)
+        sendPatch(book)
+    })
+}
+
+//not sending PATCH correctly
+function sendPatch(book){
+        fetch(`http://localhost:3000/books/${book.id}`, {
+            method: 'PATCH',
+            headers:{
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                users: book.username
+            })
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
